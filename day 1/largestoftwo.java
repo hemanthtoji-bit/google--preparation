3727f92cee77459d9fac24fdd353423b4ee653df
hemanthtoji-bit <hemanthtoji@gmail.com>
2026-08-16 00:24:22 +0530

    Rename class from lo2 to largestoftwo

--- day 1/largestoftwo.java
+++ day 1/largestoftwo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class lo2 {
+public class largestoftwo {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter two numbers");
