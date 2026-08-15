c67b1c89b4c2b2419076a39394ff7f4c9eebdfcc
hemanthtoji-bit <hemanthtoji@gmail.com>
2026-08-16 00:23:59 +0530

    Rename class from 'lo3' to 'largestofthree'

--- day 1/largestofthree.java
+++ day 1/largestofthree.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class lo3{
+public class largestofthree{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter three numbers");
