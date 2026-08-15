c9b211e3b6292dc9d1df7409efd4f727b6ece230
hemanthtoji-bit <hemanthtoji@gmail.com>
2026-08-16 00:13:12 +0530

    Add Java program to calculate sum of first n numbers

---
+++ day 1/sum of n.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class sumofn {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println("The sum of first "+n+" numbers is: "+sum);  
+    }
+    
+}
