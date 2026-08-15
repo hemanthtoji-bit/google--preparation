50ce63bf4e6363d2bf0b2d9b4ffc11ee8002c4c0
hemanthtoji-bit <hemanthtoji@gmail.com>
2026-08-16 00:12:35 +0530

    Add Java program to print numbers up to user input

---
+++ day 1/printnnum.java
@@ -0,0 +1,18 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+import java.util.Scanner;
+
+public class nnum {
+   public nnum() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter a number");
+      int var2 = var1.nextInt();
+
+      for(int var3 = 1; var3 <= var2; ++var3) {
+         System.out.println(var3);
+      }
+
+   }
+}
