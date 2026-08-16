7bea650969552002a6c11f36ea39e2b87a9d2700
hemanthtoji-bit <hemanthtoji@gmail.com>
2026-08-16 21:19:54 +0530

    Create printarr.java

---
+++ day2/printarr.java
@@ -0,0 +1,9 @@
+public class printarr {
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+        System.out.println("The elements of the array are: ");
+        for(int m: arr){
+             System.out.println(m);
+        }
+    }
+}
