730ed1a7dcf743d1dea0309d6ae71a2b00dc6b69
hemanthtoji-bit <hemanthtoji@gmail.com>
2026-08-16 21:30:27 +0530

    Implement array input and maximum value finder
    Fix input handling and find maximum in array.

---
+++ day2/largeofarr.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class largeofarr{
+  public static void main(String[] args){
+                   Scanner sc=new Scanner(System.in);
+                   System.out.println("enter number of elements");
+                   int n= sc.nextInt[];
+                   int arr[]=new int[n];
+           for(int i=0;i<arr.length;i++){
+                       arr[i]=sc.nextInt[];
+           }
+    int max=arr[0];
+          for(int i=0;i<arr.length;i++){
+                 if(arr[i]>max){
+                      max=arr[i];
+                 }
+          }
+    System.out.println("the maximum element in arr is" + max);
+
+  }
+
+}
+
+                  
+                  
+             
