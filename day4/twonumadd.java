85a98bf34f1fc76f2b893d4eb0d1cfdfc3ed7424
hemanthtoji-bit <hemanthtoji@gmail.com>
2026-08-18 23:53:02 +0530

    Add Java program to sum two numbers

---
+++ day4/twonumadd.java
@@ -0,0 +1,18 @@
+public class twonumsum{
+     static int add(int a,int b){
+                return a+b;
+             }
+    public static void main(String[] args){
+            
+             int result=add(10,20);
+             System.out.println("The sum is: " + result);
+
+
+
+
+
+    }
+
+
+
+}
